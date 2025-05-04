edc9c85faab213eb80ecdcceeec0ce0124bfb470
Martin Riziky <martinrizling@gmail.com>
2025-05-04 11:31:24 +0200

    Pridané zmeny po testovaní a nasadení

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
